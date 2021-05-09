f67d8aed612c494adf86b312ea53f52ef3db7e7a
quangg@wd <quanggtangg@gmail.com>
2021-05-09 14:43:10 +0700

    ui: completed UI

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent implements OnInit {
+  isHovering = false;
+  isEditing = false;
 
   constructor() { }
 
